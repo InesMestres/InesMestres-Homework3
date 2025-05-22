1cab6b25d32d9a12d9329efda20bb26cb75bec2f
InesMestres <imestres@udesa.edu.ar>
2025-05-22 16:02:23 -0300

    main 2

---
+++ Ejercicio2/Main2.cpp
@@ -0,0 +1,16 @@
+#include "Ejercicio2.hpp"
+
+int main(){
+
+    Circulo circuloOrigen();
+    Elipse elipseOrigen();
+    Rectangulo rectanguloOrigen();
+
+    circuloOrigen.calcularArea();
+    elipseOrigen.calcularArea();
+    rectanguloOrigen.calcularArea();
+
+    Punto punto()
+
+    return 0;
+}
