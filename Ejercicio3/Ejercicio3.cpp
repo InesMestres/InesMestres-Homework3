5e356283863e3689dacd234dc1cc85ecc582dac5
InesMestres <imestres@udesa.edu.ar>
2025-05-24 19:26:42 -0300

    cambios

--- Ejercicio3/Ejercicio3.cpp
+++ Ejercicio3/Ejercicio3.cpp
@@ -17,7 +17,7 @@ Clase1<T>::Clase1(const vector<T>& vectorCreado): vectorExistente(vectorCreado){
 
 template<typename T>
 void Clase1<T>::agregar(const T& valor){
-    Vector.push_back(valor);
+    vectorExistente.push_back(valor);
     return;
 }
 
@@ -27,25 +27,25 @@ std::string Clase1<T>::procesar(){
 
     if constexpr(is_same<T, string>:: value){
         stringOutput << "[";
-        for(int i = 0; i < Vector.size(); i++){
-            stringOutput << Vector[i];
+        for(int i = 0; i < vectorExistente.size(); i++){
+            stringOutput << vectorExistente[i];
         }
         stringOutput << "]";
     }
 
     if constexpr(is_same<T, double>:: value){
         stringOutput << "[";
-        for(int i = 0; i < Vector.size(), i++){
-            stringOutput << Vector[i];
+        for(int i = 0; i < vectorExistente.size(), i++){
+            stringOutput << vectorExistente[i];
         }
         stringOutput << "]";
     }
 
-    if constexpr(is_same<T, Vector<int>>:: value){
-        for(int i = 0; i < Vector.size(), i++){
+    if constexpr(is_same<T, vectorExistente<int>>:: value){
+        for(int i = 0; i < vectorExistente.size(), i++){
             stringOutput << "[";
-            for(int j = 0; j < Vector[i].size(), j++){
-                stringOutput << Vector[i][j];
+            for(int j = 0; j < vectorExistente[i].size(), j++){
+                stringOutput << vectorExistente[i][j];
             }
         stringOutput << "]";
         }
